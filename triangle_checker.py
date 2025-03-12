bc8ea41cc3ecd9c1acf633894e70553ecfa88183
Dumisani Mathabathe <dumimatha3@gmail.com>
2025-03-13 01:55:13 +0200

    Delete triangle_checker.py

--- triangle_checker.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Enter the first side:\n"))
-b = int(input("Enter the second side:\n"))
-c = int(input("Enter the third side:\n"))
-
-if a + b > c and a + c > b and b + c > a:
-    print("The given sides form a valid triangle.")
-else:
-    print("The given sides do NOT form a valid triangle.")
